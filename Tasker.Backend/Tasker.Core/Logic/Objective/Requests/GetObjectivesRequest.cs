e1d7c6f60191b357f401d118823b9b37fadf9175
artemkarbanovich <artyomkarbanovich@gmail.com>
2022-07-10 23:31:28 +0300

    pre Quartz.NET adding

--- Tasker.Backend/Tasker.Core/Logic/Objective/Requests/GetObjectivesRequest.cs
+++ Tasker.Backend/Tasker.Core/Logic/Objective/Requests/GetObjectivesRequest.cs
@@ -6,7 +6,7 @@ public class GetObjectivesRequest
     private const int MaxPageSize = 15;
     private int pageSize = 5;
 
-    public int PageNumber { get; set; } = 1;
+    public int PageNumber { get; set; } = 0;
     public int PageSize
     {
         get => pageSize;
